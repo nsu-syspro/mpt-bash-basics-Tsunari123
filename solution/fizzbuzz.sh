f24aa5bab5b4bc36995ceaba6716c0322eca657c
Tsunari123 <grumiktiktok@gmail.com>
2025-10-02 00:53:34 +0800

    Commit

---
+++ solution/fizzbuzz.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+for i in $(seq 1 "$1"); do
+	if (( i % 15 == 0 )); then
+		echo "Fizz Buzz"
+	elif (( i % 3 == 0 )); then
+		echo "Fizz"
+	elif (( i % 5 == 0 )); then
+		echo "Buzz"
+	else
+		echo "$i"
+	fi
+done
